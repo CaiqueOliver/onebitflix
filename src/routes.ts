87f74a9c9938d9380903a084acc1aea6781a20ac
Caique Oliveira <caique.silva.ext@infracommerce.com.br>
2023-02-08 14:18:07 -0300

    feat: get info from courses

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { categoriesCcntroller } from "./controllers/categoriesController";
+import { coursesController } from "./controllers/coursesController";
 const router = express.Router();
 router.get("/categories", categoriesCcntroller.index);
 router.get("/categories/:id", categoriesCcntroller.show);
+router.get("/courses/:id", coursesController.show);
 
 export { router };
