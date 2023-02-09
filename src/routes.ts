3cee7acba0716a094a9b012838d097a1c0d73862
Caique Oliveira <caique.silva.ext@infracommerce.com.br>
2023-02-09 11:18:17 -0300

    feat: streaming video

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { categoriesCcntroller } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
+import { episodesController } from "./controllers/episodesController";
 const router = express.Router();
 router.get("/categories", categoriesCcntroller.index);
 router.get("/categories/:id", categoriesCcntroller.show);
@@ -9,5 +10,6 @@ router.get("/courses/featured", coursesController.featured);
 router.get("/courses/search", coursesController.search);
 router.get("/courses/newest", coursesController.newest);
 router.get("/courses/:id", coursesController.show);
+router.get("/episodes/stream", episodesController.stream);
 
 export { router };
